cefeae249c25586bc5c4e0929ed6dcc86e53bc9a
Danylo Kolumbet <kolumbetko@gmail.com>
2022-05-20 19:16:47 +0300

    Refactor listeners.js file into Input class

--- init.js
+++ init.js
@@ -1,11 +1,21 @@
 "use strict";
 const canvas = document.querySelector("#Canvas");
 const ctx = canvas.getContext("2d", { alpha: false });
-let width, height, lesser, bigger;
 let pause = true;
 let lastFrameTime = 0;
 // noise.seed(0);
 
-updateSize();
+let width;
+let height;
+let lesser;
+let bigger;
+Screen.updateSize();
 
 frame();
+
+window.addEventListener(EVENT.resize, Screen.resizeHandler);
+canvas.addEventListener(EVENT.pointerdown, Input.pointerdownHandler);
+canvas.addEventListener(EVENT.pointermove, Input.pointermoveHandler);
+window.addEventListener(EVENT.pointerup, Input.pointerupHandler);
+window.addEventListener(EVENT.keydown, Input.keydownHandler);
+window.addEventListener(EVENT.keyup, Input.keyupHandler);
