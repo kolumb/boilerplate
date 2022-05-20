4556ef280d7f4692d72c02147ddb286fe8f7e47a
Danylo Kolumbet <kolumbetko@gmail.com>
2022-05-20 19:14:24 +0300

    Define time and FPS constants

--- frame.js
+++ frame.js
@@ -1,4 +1,7 @@
 "use strict";
+const TARGET_FPS = 60;
+const SECOND = 1000;
+
 function tick(dt) {}
 function render() {
     ctx.fillStyle = pause ? "rgb(200,200,200)" : "rgb(240,240,240)";
@@ -9,7 +12,7 @@ function frame(timestamp) {
     const dt = timestamp - lastFrameTime;
     lastFrameTime = timestamp;
 
-    if (dt < 1000) tick(dt * 0.06);
+    if (dt < SECOND) tick(dt * TARGET_FPS / SECOND);
     render();
     if (pause === false) {
         requestAnimationFrame(frame);
